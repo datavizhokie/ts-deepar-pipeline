e92155bda005a4ade443238b4070ff49563ee460
Matt Wheeler <Matthew.Wheeler19@T-Mobile.com>
2021-01-21 11:33:15 -0700

    + main()

--- deepar_prep.py
+++ deepar_prep.py
@@ -35,8 +35,6 @@ def read_create_mindate(file, series_field, granularity):
 
     return df_joined
 
-df_joined = read_create_mindate(file='grouped_data.csv', series_field='OFFENSE_CATEGORY_ID', granularity='year_week')
-
 
 def train_split(df, max_train_date, series_field, granularity):
 
@@ -52,8 +50,6 @@ def train_split(df, max_train_date, series_field, granularity):
         f.min(granularity), f.max(granularity)).show()
 
     return train_data, test_data
-
-train_data, test_data = train_split(df=df_joined, max_train_date="2020-01", series_field='OFFENSE_CATEGORY_ID', granularity='year_week')
 
 
 def create_array_formats(train_data, test_data, series_field, granularity):
@@ -81,21 +77,30 @@ def create_array_formats(train_data, test_data, series_field, granularity):
     print("Preview of DF with Target arrays by Series:")
     sorted_list_train.show(5)
 
-
     #TODO: add logic for Cat's and DynFeat's
     # train_final = sorted_list_train.select("OFFENSE_CATEGORY_ID","start","target").persist()
                                        #f.array(["cat2"]).alias("cat"),
                                                     #f.array(["dynamic_feat1","dynamic_feat2"]).alias("dynamic_feat")).persist()
+    # test_final = ...
 
     return sorted_list_train, sorted_list_test
 
 
-sorted_list_train, sorted_list_test = create_array_formats(train_data=train_data, test_data=test_data, series_field='OFFENSE_CATEGORY_ID', granularity='year_week')
-
-
-def write_final_to_json(train, test):
+def write_final_to_json(train, test, pathkey):
     #TODO: write to S3
-    train.coalesce(1).write.mode('overwrite').json('training_input/train/')
-    test.coalesce(1).write.mode('overwrite').json('training_input/test/')
+    try:
+        train.coalesce(1).write.mode('overwrite').json(f'{pathkey}/train/')
+        test.coalesce(1).write.mode('overwrite').json(f'{pathkey}/test/')
+        print("Write to json succeeded.")
+    except:
+        print("Write to json failed.")
 
-write_final_to_json(sorted_list_train, sorted_list_test)
+def main():
+
+    df_joined = read_create_mindate(file='grouped_data.csv', series_field='OFFENSE_CATEGORY_ID', granularity='year_week')
+    train_data, test_data = train_split(df=df_joined, max_train_date="2020-01", series_field='OFFENSE_CATEGORY_ID', granularity='year_week')
+    sorted_list_train, sorted_list_test = create_array_formats(train_data=train_data, test_data=test_data, series_field='OFFENSE_CATEGORY_ID', granularity='year_week')
+    write_final_to_json(train=sorted_list_train, test=sorted_list_test, pathkey='training_input')
+
+if __name__ == "__main__":
+    main()
